4e1331effeb01e502a0af3455fb9013911841ee4
Miha Krkovic <miha.krkovic@yahoo.com>
2023-01-22 18:00:03 +0100

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header(''Breakfast Menu)
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 6 Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 strreamlit.text('Hard-Boiled Free-Range Egg')
