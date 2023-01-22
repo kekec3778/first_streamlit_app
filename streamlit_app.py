f245a78612af2cf8563cbdc073b64454b53d468f
Miha Krkovic <miha.krkovic@yahoo.com>
2023-01-22 17:59:34 +0100

    Update streamlit_app.py
    Added header anf other text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header(''Breakfast Menu)
+streamlit.text('Omega 3 6 Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+strreamlit.text('Hard-Boiled Free-Range Egg')
+
